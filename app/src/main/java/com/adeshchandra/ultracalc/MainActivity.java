75b54b3639364e62fc7284605d4520b606dd3142
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-05-20 08:22:52 +0600

    Update MainActivity.java

--- app/src/main/java/com/adeshchandra/ultracalc/MainActivity.java
+++ app/src/main/java/com/adeshchandra/ultracalc/MainActivity.java
@@ -11,7 +11,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         int[] cards = {R.id.cardCalc, R.id.cardSci, R.id.cardConv, R.id.cardFin, R.id.cardHealth, R.id.cardMath, R.id.cardDate, R.id.cardHistory};
-        Class<?>[] acts = {CalcActivity.class, ScientificActivity.class, ConverterActivity.class, FinancialActivity.class, HealthActivity.class, MathActivity.class, DateCalcActivity.class, HistoryActivity.class};
+        Class<?>[] acts = {
+            CalcActivity.class, 
+            ScientificActivity.class, 
+            ConverterActivity.class, // This will now hold our Wood & Universal Converter
+            FinancialActivity.class, 
+            HealthActivity.class, 
+            MathActivity.class, 
+            DateCalcActivity.class, 
+            HistoryActivity.class
+        };
         for (int i = 0; i < cards.length; i++) {
             final Class<?> act = acts[i];
             findViewById(cards[i]).setOnClickListener(v -> startActivity(new Intent(this, act)));
